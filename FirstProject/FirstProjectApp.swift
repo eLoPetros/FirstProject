71950bdc3dcef87f038eee21acb92adb5ea8ff3e
Petros <emlq@mail.ru>
2022-11-11 14:06:36 +0600

    Initial Commit

---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by Петрос Каратэги on 11.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
